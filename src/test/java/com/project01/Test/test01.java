f37c0eef98c6a46b86183ad5a23125b9c42760bf
oguz <ouz1571@gmail.com>
2020-03-02 14:13:18 +0100

    added

---
+++ src/test/java/com/project01/Test/test01.java
@@ -0,0 +1,39 @@
+package com.project01.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test01 {
+
+    public static void main(String [] args) throws InterruptedException {
+
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.amazon.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        Thread.sleep(3000);
+        driver.close();
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
